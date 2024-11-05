00edca95b26d964a5ec8725104a4fa0acce1b7ad
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-11-05 17:14:19 -0300

    criando o Node da arvoreB

---
+++ BTree/B/NodeB.java
@@ -0,0 +1,37 @@
+package B;
+
+
+public class NodeB {
+
+    private int n;
+    private int[] infos;
+    private NodeB[]filhos;
+
+    NodeB(int m){
+        this.infos = new int[m];
+        this.filhos = new NodeB[m];
+    }
+
+    int getN(){
+        return this.n;
+    }
+
+    void setN(int n){
+        this.n = n;
+    }
+
+    int getInfo(int pos){
+        return this.infos[pos];
+    }
+    NodeB getFilho(int pos){
+        return this.filhos[pos];
+    }
+    
+    void setInfo(int pos, int val){
+        this.infos[pos] = val;
+    }
+    void setFilho(int pos, NodeB filho){
+        this.filhos[pos] = filho; 
+    }
+
+}
