54e466b24b8a601b70a380facf083d0da53f261a
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-11-23 23:47:52 -0300

    copiando

--- ArvoreB/BTree/BTree.java
+++ ArvoreB/BTree/BTree.java
@@ -1,5 +1,42 @@
 package BTree;
 
-public class BTree {
+public class BTree<T extends Comparable<T>> {
+    private int m;
+    private BNode<T> root;
+
+    BTree(){
+        this.m = 5;
+        this.root = null;
+    }
+
+    public boolean isEmpty(){
+        return this.root == null;
+    }
+
+    
+
+
+    public void insert(T k){
+        if(this.isEmpty()){
+            BNode<T> novo = new BNode<T>(m);
+            novo.setInfo(0, k);
+            novo.setN(novo.getN()+1);
+            this.root = novo;
+        }
+        else{
+            BNode<T> aux = root;
+            int i = 0;
+            while(true){
+                if(k.compareTo(aux.getInfo(i)) < 0){
+
+                }
+                else if(k.compareTo(aux.getInfo(i)) == 0){
+                    System.out.println("Valor repetido!");
+                }
+            }
+        }
+
+    }
+
     
 }
