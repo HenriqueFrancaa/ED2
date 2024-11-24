31ec75ba7f00e8976fc7ef477dfaecbfe6af04c0
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-11-24 19:22:08 -0300

    atualizando

--- BTree/B/App.java
+++ BTree/B/App.java
@@ -1,14 +1,12 @@
+import BTree.BTree;
 
-public class App{
+public class App {
     public static void main(String[] args) {
-        BTree arv = new BTree();
-
-        int v[] = {5,10,25,40,9,20,80};
-
-        for(int i = 0; i < v.length; i++){
-            arv.insert(v[i]);
+        BTree<Integer> arv = new BTree<Integer>();
+        int x[] = { 12, 27, 54, 86, 33, 41, 67, 72, 94, 29, 23, 38, 59, 107, 61, 32, 131 };
+        for (int i = 0; i < x.length; i++) {
+            arv.insert(x[i]);
         }
-
-        arv.acharMaior();
+        arv.levelTour();
     }
 }
---
+++ BTree/B/BTree/BNode.java
@@ -0,0 +1,58 @@
+package BTree;
+
+class BNode<T extends Comparable<T>> {
+    private BNode<T> pai; // pai
+    private T[] infos; // chaves
+    private BNode<T>[] filhos; // filhos
+    private boolean folha; // folha
+    private int n;
+
+    BNode(int m) {
+        this.pai = null;
+        this.infos = (T[]) new Comparable[m];
+        this.filhos = new BNode[m];
+        this.folha = true;
+        this.n = 0;
+    }
+
+    void setPai(BNode<T> pai) {
+        this.pai = pai;
+    }
+
+    BNode<T> getPai() {
+        return this.pai;
+    }
+
+    void setInfo(int pos, T k) {
+        this.infos[pos] = k;
+    }
+
+    T getInfo(int pos) {
+        return this.infos[pos];
+    }
+
+    void setFilho(int pos, BNode<T> filho) {
+        this.filhos[pos] = filho;
+    }
+
+    BNode<T> getFilho(int pos) {
+        return this.filhos[pos];
+    }
+
+    void setFolha(boolean val) {
+        this.folha = val;
+    }
+
+    boolean getFolha() {
+        return this.folha;
+    }
+
+    void setN(int n) {
+        this.n = n;
+    }
+
+    int getN() {
+        return this.n;
+    }
+
+}
